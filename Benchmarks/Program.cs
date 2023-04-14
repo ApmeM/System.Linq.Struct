82007793d18ed4806ee1944754378bc5af384efd
Artem Votintsev <artem.votincev@gmail.com>
2023-04-14 11:37:38 +0200

    Add benchmark

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using CodegenAnalysis;
+using CodegenAnalysis.Benchmarks;
+using Iced.Intel;
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+BenchmarkRunner.Run<DifferentLengths>();
